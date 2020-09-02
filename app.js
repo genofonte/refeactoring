2bd2eaae3e1406a34c16b0d0056a73052a309ceb
jose <joseantonio.diaz@tecnomatrix.com>
2020-09-02 17:26:09 +0200

    final part one

--- app.js
+++ app.js
@@ -24,28 +24,41 @@ function playFor(aPerfomance) {
     return plays[aPerfomance.playID];
 }
 
+function volumeCreditsFor(aPerfomance) {
+  let result = 0;
+  result += Math.max(aPerfomance.audience - 30, 0);
+  if ("comedy" === playFor(aPerfomance).type) result += Math.floor(aPerfomance.audience / 5);
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+                      { style: "currency", currency: "USD",
+                        minimumFractionDigits: 2 }).format(aNumber);
+}
+
+function totalVolumeCredits() {
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits;
+}
+
 function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-                          { style: "currency", currency: "USD",
-                            minimumFractionDigits: 2 }).format;
   
     for (let perf of invoice.performances) {
-      let thisAmount= amountFor(perf);
-  
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
   
       // print line for this order
-      result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+      result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
       totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+
+    result += `Amount owed is ${usd(totalAmount/100)}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
   }
 
